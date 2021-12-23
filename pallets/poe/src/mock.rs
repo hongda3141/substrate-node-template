69825a2be63daca652d8160ab0e697eb1cdccffc
hongda <hongda3141@163.com>
2021-12-23 08:28:47 -0800

    feature: the 1-1 home work
    no.1 home work
    
    simple version of the no.1 home work
    
    feat: add poe
    
    modify
    
    fix: substrate tutorials error fix
    
    add modify files for system backup:
    
    test: first version test for claim length check
    
    test: simple version of the no.1 home work
    
    feat: add poe
    
    modify
    
    fix: substrate tutorials error fix
    
    add modify files for system backup:
    
    test: first version test for claim length check
    
    feat: add poe
    
    add modify files for system backup:
    
    docs: add result of home work
    
    refactor: remove the const variable
    
    style: remove some comment
    
    refactor: change test variables
    
    change the vec variable

---
+++ pallets/poe/src/mock.rs
@@ -0,0 +1,68 @@
+use crate as pallet_poe;
+use crate::{MaxLength, MinLength};
+use frame_support::parameter_types;
+use frame_system as system;
+use sp_core::H256;
+use sp_runtime::{
+	testing::Header,
+	traits::{BlakeTwo256, IdentityLookup},
+};
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+type Block = frame_system::mocking::MockBlock<Test>;
+
+// Configure a mock runtime to test the pallet.
+frame_support::construct_runtime!(
+	pub enum Test where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+		PoeModule: pallet_poe::{Pallet, Call, Storage, Event<T>},
+	}
+);
+
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
+	pub const SS58Prefix: u8 = 42;
+}
+
+// 实现系统模块的config接口 for Test of runtime
+impl system::Config for Test {
+	type BaseCallFilter = frame_support::traits::Everything;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type DbWeight = ();
+	type Origin = Origin;
+	type Call = Call;
+	type Index = u64;
+	type BlockNumber = u64;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = u64; // notice this
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = Event;
+	type BlockHashCount = BlockHashCount;
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = ();
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = SS58Prefix;
+	type OnSetCode = ();
+}
+
+impl pallet_poe::Config for Test {
+	type Event = Event;
+	type MaxLength = MaxLength;
+	type MinLength = MinLength;
+}
+
+// Build genesis storage according to the mock runtime.
+// 测试用的链上环境和初始化创世块配置
+pub fn new_test_ext() -> sp_io::TestExternalities {
+	system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
+}
